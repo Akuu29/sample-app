8da9d41d46a0c3b1f195c799f86a761f73527c35
Akuu29 <owner.akiu0529@gmail.com>
2022-03-23 06:12:09 +0900

    add add "listenfd", "dotenv" road port & host from .env

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 use actix_web::{App, HttpServer, get, Responder};
+use dotenv::dotenv;
+use listenfd::ListenFd;
+use std::env;
 
 #[get("/hello")]
 async fn hello_world() -> impl Responder {
@@ -7,11 +10,24 @@ async fn hello_world() -> impl Responder {
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    dotenv().ok(); // 初期化
+
+    let mut listenfd = ListenFd::from_env();
+    let mut server = HttpServer::new(|| {
         App::new()
           .service(hello_world)
-    })
-      .bind("127.0.0.1:3000")?
-      .run()
-      .await
+    });
+
+    // systemfdによってwatchしていた場合、そのhostとportを使用
+    // そうでない場合、.envから読み込んだ環境変数を使用
+    server = match listenfd.take_tcp_listener(0)? {
+        Some(listener) => server.listen(listener)?,
+        None => {
+            let host = env::var("HOST").expect("Please set host in .env");
+            let port = env::var("PORT").expect("Please set post in .env");
+            server.bind(format!("{}:{}", host, port))?
+        }
+    };
+
+    server.run().await
 }
