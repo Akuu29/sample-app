86263976c9b2d29fffc0194732b672daf636ba5a
Akuu29 <owner.akiu0529@gmail.com>
2022-03-23 05:00:50 +0900

    update add function "hello_world".

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
-use actix_web::{App, HttpServer};
+use actix_web::{App, HttpServer, get, Responder};
+
+#[get("/hello")]
+async fn hello_world() -> impl Responder {
+    format!("Hello World!")
+}
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
+          .service(hello_world)
     })
-    .bind("127.0.0.1:3000")?
-    .run()
-    .await
+      .bind("127.0.0.1:3000")?
+      .run()
+      .await
 }
