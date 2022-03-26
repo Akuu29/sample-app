0019b4fede39f4ae7cf5a505c4cc48ff7870f447
Akuu29 <owner.akiu0529@gmail.com>
2022-03-26 15:50:04 +0900

    fix fix db.rs add crate "diesel_maigration"

--- src/db.rs
+++ src/db.rs
@@ -5,7 +5,6 @@ use diesel::mysql::MysqlConnection;
 use diesel::r2d2::ConnectionManager;
 // staticなglobal変数を非同期に設定できるようにするmodule
 use lazy_static::lazy_static;
-use r2d2;
 use std::env;
 
 type Pool = r2d2::Pool<ConnectionManager<MysqlConnection>>;
@@ -16,20 +15,20 @@ pub type DbConnection = r2d2::PooledConnection<ConnectionManager<MysqlConnection
  * initないで呼ばれるrunによって、コネクションを作成した際にmigrationを流す必要がある場合、
  * 流してくれる。
  */
-embed_migration!();
+embed_migrations!();
 
 lazy_static! {
     static ref POOL: Pool = {
         let db_url = env::var("DATABASE_URL").expect("Please set Database Url in .env");
-        let manager = ConnectionManagaer::<MysqlConnection>::new(db_url);
-        Pool::new(manager).expect("Failed to create db pool");
+        let manager = ConnectionManager::<MysqlConnection>::new(db_url);
+        Pool::new(manager).expect("Failed to create db pool")
     };
 }
 
 pub fn init() {
     lazy_static::initialize(&POOL);
     let conn = connection().expect("Failed to get db connection");
-    embedded_migration::run(&conn).unwrap();
+    embedded_migrations::run(&conn).unwrap();
 }
 
 pub fn connection() -> Result<DbConnection, CustomError> {
