5e259f14b455388e53eea28c760180bfb3f0aa60
Akuu29 <owner.akiu0529@gmail.com>
2022-03-29 21:02:06 +0900

    add add templates/ update fn find_all in src/todos/routes.rs

--- src/todos/routes.rs
+++ src/todos/routes.rs
@@ -1,13 +1,33 @@
-use actix_web::{get, post, put, delete, web, HttpResponse};
+use actix_web::{get, post, put, delete, web, HttpResponse, ResponseError, http::header};
 use crate::todos::{Todo, Todos};
 use crate::error_handler::CustomError;
 use serde_json::json;
+use askama::Template;
+use thiserror::Error;
+
+#[derive(Template)]
+#[template(path="index.html")]
+struct IndexTemplate {
+    todos:  Vec<Todos>,
+}
+
+#[derive(Error, Debug)]
+enum MyError {
+    #[error("Filed to render HTML")]
+    AskamaError(#[from]askama::Error),
+}
+
+impl ResponseError for MyError {}
 
 // 全件取得
-#[get("/todos")]
-async fn find_all() -> Result<HttpResponse, CustomError> {
-    let todos = Todos::find_all()?;
-    Ok(HttpResponse::Ok().json(todos))
+#[get("/")]
+async fn find_all() -> Result<HttpResponse, MyError> {
+    let todos = Todos::find_all().unwrap();
+    let html = IndexTemplate {todos};
+    let response_body = html.render()?;
+    Ok(HttpResponse::Ok()
+      .content_type("text/html")
+      .body(response_body))
 }
 
 // idより取得
